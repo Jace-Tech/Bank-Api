18173bfc00fb8b0cfb0c1735ab81f95c55db030c
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-01-06 11:56:34 +0100

    removed the build folder

--- build/utils/functions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateAccountNumber = void 0;
-const generateAccountNumber = () => Math.floor(Math.random() * 1000000000);
-exports.generateAccountNumber = generateAccountNumber;
--- build/utils/response.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.response = void 0;
-const response = (message, data = null, success = true) => {
-    return { message, data, success };
-};
-exports.response = response;
