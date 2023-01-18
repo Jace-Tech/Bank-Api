355e351ae24359bb6e007d848292a55b286db05d
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-01-18 05:05:47 +0100

    updated the package json again

--- build/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const router = (0, express_1.Router)();
-// Register Route
-router.post("/sign-up", auth_controller_1.handleSignUp);
-router.post("/sign-in", auth_controller_1.handleSignIn);
-exports.default = router;
