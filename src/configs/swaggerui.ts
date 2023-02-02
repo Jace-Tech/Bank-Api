fe1ce0563af02ee2484be4a64be3f4f6062c718e
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-02 01:29:51 +0100

    added the upload route

--- src/configs/swaggerui.ts
+++ src/configs/swaggerui.ts
@@ -1,36 +1,8 @@
 import { Express } from 'express';
-import swaggerJsdoc from "swagger-jsdoc"
 import swaggerUi from "swagger-ui-express"
+import docs from "./../swagger.json"
 
 export default (app: Express) => {
-  const options = {
-    definition: {
-      openapi: "3.1.0",
-      swagger: "2.0",
-      info: {
-        title: "BANK API",
-        version: "1.0.0",
-        description:
-          "BANK API documentation",
-        license: {
-          name: "MIT",
-        },
-        contact: {
-          name: "Chidindu Emmanuel Aneke [JACE]",
-          url: "https://github.com/Jace-Tech",
-          email: "jacedev151@gmail.com",
-        },
-      },
-      servers: [
-        {
-          url: "http://localhost:5000",
-        },
-      ],
-    },
-    apis: ["src/models/*.ts", "build/models/*js"],
-  };
-
-  const specs = swaggerJsdoc(options);
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(docs));
   return app;
 }
